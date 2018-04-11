9a7126ecd51d4e85c4447bb67522f0f524a6063e
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-04-10 22:19:27 -0400

    Create basic_loop.js

---
+++ Lecture_13/class_notes/basic_loop.js
@@ -0,0 +1,30 @@
+// for(let i = 2; i < 6; i = i + 2) {
+//     console.log(i)
+// }
+
+const ar = ['a', 'b', 'c'];
+
+// ar[0]
+// ar[1]
+// ar[2]
+
+// let i = 0;
+
+// ar[i]
+// i = i + 1;
+
+// ar[i]
+// i = i + 1
+
+// ar[i]
+// i = i + 1
+
+i = 0;
+while (i < ar.length) {
+    console.log(ar[i])
+    i = i + 1;
+}
+
+for (i = 0; i < ar.length; i++) {
+    console.log(ar[i])
+}
