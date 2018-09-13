0ad3607bd193ef64088694e6168f5f304499f3b7
Mottaqui Karim <mottaquikarim@users.noreply.github.com>
2018-09-13 09:39:19 -0400

    Add files via upload

---
+++ Lecture_9/class_notes/jsbasics/js/app.js
@@ -0,0 +1,68 @@
+let mycash = 20
+console.log('starting! my cash is', mycash)
+let steak = 5
+mycash = mycash - steak
+console.log('did something! my cash is', mycash)
+
+// this is a single line comment in javascript
+
+/*
+	this is
+		a 
+			multilined comment
+
+	in javascript
+*/
+
+// this is a number
+console.log(1, typeof 1)
+
+// this is a string
+console.log("hello, class", typeof "hello, class" )
+
+// this is ALSO a string
+console.log('hello, class with single quotes')
+
+// this is ALSO ALSO a string
+console.log(`yes, there are three ways to define strings in js`)
+
+// this is a boolean
+console.log(true, typeof true)
+console.log(false, typeof false)
+
+
+// javscript numeric operators
+console.log(1+1) // addition
+console.log(2-1) // subtraction
+console.log(3*2) // multiplication
+console.log(4/2) // division
+console.log(4 ** 5) // exponent
+console.log(5 % 2) // modulus operator
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
